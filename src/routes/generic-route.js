46858bed16041ae5de5a5c3f96feb3a527114dcb
RockatanskyV8 <luccasanttoz@gmail.com>
2019-12-24 16:26:00 -0300

    altomatizando a inclusão de novos arquivos

--- src/routes/generic-route.js
+++ src/routes/generic-route.js
@@ -3,15 +3,24 @@
 const express    = require('express');
 const controller = require('../controller/controller');
 const router     = express();
+const fs         = require('fs');
 
-const paths =
-{
-  'mssg' : require('./mssg/mssg')
-}
+const listFilesIn = ( path ) => fs.readdirSync( path )
+const removeJSExtension = ( action ) => action.replace( '.js', '' )
+
+const createAction = ( action ) => ( {
+  [ action ]: require( `./actions/${ action }` )
+} )
+
+const toController = ( obj, action ) => Object.assign( obj, createAction( action ) )
+const createController = ( actions ) => actions.reduce( toController, {} )
+
+const fileList = (listFilesIn( './src/routes/actions' ).map( removeJSExtension ))
+const paths = createController( fileList )
 
 const routes = [
-  { path: '/mssg/users', action: paths['mssg'] },
-  { path: '/mssg/posts', action: paths['mssg'] }
+  { path: '/mssg/users', action: paths.mssg },
+  { path: '/mssg/posts', action: paths.mssg }
 ]
 
 const createRoute  = ( route )  => router[ 'use' ]( route.path, route.action )
